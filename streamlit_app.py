af7920e7f4a112c099094661c3a02c6b1ba3a583
dataJH <89939933+dataJH@users.noreply.github.com>
2022-11-29 14:49:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,9 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#allow end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', add_my_fruit)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#streamlit.text(fruityvice_response) #outputs to screen
